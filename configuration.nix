a25d5400c08e1ddcc638f66a21870fdc79df314e
James4630 <mail@elliotkirby.de>
2025-11-09 20:56:12 +0100

    optimized velocity

--- configuration.nix
+++ configuration.nix
@@ -444,6 +444,7 @@
   				autoStart = true;
   				#restart = "no";
   				package = pkgs.velocityServers.velocity;
+  				jvmOpts = "-XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15";
   			};
   		};
   	};
