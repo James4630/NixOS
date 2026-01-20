df2f62dd5e300ca8019b4c32447450a84ec7dc8e
James4630 <mail@elliotkirby.de>
2026-01-20 20:14:08 +0100

    updated to 25.11 and pinned 25.05 for seafile

--- flake.nix
+++ flake.nix
@@ -2,11 +2,12 @@
   description = "NixOS Config Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11-small";
+    nixpkgs-2505.url = "github:nixos/nixpkgs/nixos-25.05-small";
     nix-minecraft.url = "github:Infinidoge/nix-minecraft";
   };
 
-  outputs = { self, nixpkgs, ...  }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-2505, ...  }@inputs: {
     nixosConfigurations = {
     	monolith = nixpkgs.lib.nixosSystem {
     		specialArgs = {inherit inputs;};
