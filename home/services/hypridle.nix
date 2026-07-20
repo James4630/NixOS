5b71527fdbe7c16e884b50e58ed3911945c0b54b
James4630 <mail@elliotkirby.de>
2026-07-20 11:47:15 +0200

    updated and disbaled automatic locking for KiFeWo

--- home/services/hypridle.nix
+++ home/services/hypridle.nix
@@ -9,17 +9,17 @@
 				ignore_dbus_inhibit = false;
 				lock_cmd = "pidof hyprlock || hyprlock";
 			};
-			listener = [
-				{
-					timeout = 300;
-					on-timeout = "pidof hyprlock || hyprlock";
-				}
-				{
-					timeout = 600;
-					on-timeout = "niri msg action power-off-monitors";
-					on-resume = "niri msg action power-on-monitors";
-				}
-			];
+			#listener = [
+				#{
+				#	timeout = 300;
+				#	on-timeout = "pidof hyprlock || hyprlock";
+				#}
+				#{
+				#	timeout = 600;
+				#	on-timeout = "niri msg action power-off-monitors";
+				#	on-resume = "niri msg action power-on-monitors";
+				#}
+			#];
 		};
 	};
 }
